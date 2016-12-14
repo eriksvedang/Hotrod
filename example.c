fc4027373da011d18ea3592a9194e96a41ca845d
Erik Svedäng <erik.svedang@gmail.com>
2016-12-15 00:21:24 +0100

    Expanded a bit on the example.

--- example.c
+++ example.c
@@ -6,8 +6,12 @@
 #include "hotrod.h"
 
 int main() {
-  hotrod_print_compile_command = true; // see what compiler commands are invoked
+  hotrod_print_compile_command = true; // Turn on this flag to print compiler commands that are invoked
+
   for(int i = 0; i < 3; i++) {
     HOTROD(stuff); // will only be compiled the first time in the loop
+    stuff(); // The function can be called from other places in the same scope
   }
+
+  // The function is not accessible here, because of the for-loop's scope.
 }
